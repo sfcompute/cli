ef3ddf2f7cfb5e992ab82fe3542e92e423e6c349
flaque <evan@sfcompute.com>
2024-07-03 17:13:38 -0700

    add upgrade

--- src/lib/upgrade.ts
+++ src/lib/upgrade.ts
@@ -3,8 +3,23 @@ import type { Command } from "commander";
 export function registerUpgrade(program: Command) {
 	return program
 		.command("upgrade")
+		.argument("[version]", "The version to upgrade to")
 		.description("Upgrade to the latest version or a specific version")
-		.action(async () => {
-			await Bun.$`curl -fsSL https://sfcompute.com/cli/install | bash`;
+		.action(async (version) => {
+			if (version) {
+				const url = `https://github.com/sfcompute/cli/archive/refs/tags/${version}.zip`;
+				const response = await fetch(url, { method: "HEAD" });
+
+				if (response.status === 404) {
+					console.error(`Version ${version} does not exist.`);
+					process.exit(1);
+				}
+			}
+
+			if (version) {
+				await Bun.$`bash -c "$(curl -fsSL https://www.sfcompute.dev/cli/install)" -- ${version}`;
+			} else {
+				await Bun.$`bash -c "$(curl -fsSL https://www.sfcompute.dev/cli/install)"`;
+			}
 		});
 }
