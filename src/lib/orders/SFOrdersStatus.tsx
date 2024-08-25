c6863f18761e68d0ded4e376677ece7f285feec9
Benyam Ephrem <ephremdsg@gmail.com>
2024-08-25 12:55:38 -0700

    Update SFOrdersStatus.tsx

--- src/lib/orders/SFOrdersStatus.tsx
+++ src/lib/orders/SFOrdersStatus.tsx
@@ -4,6 +4,8 @@ import { OrderStatus, type HydratedOrder } from "../../api/orders";
 import type { Nullable } from "../../helpers/empty";
 import Spinner from "ink-spinner";
 import type { ApiError } from "../../api";
+import dayjs from "dayjs";
+import { centicentsToDollarsFormatted } from "../../helpers/units";
 
 interface SFBuyProps {
   orderId: string;
@@ -13,7 +15,12 @@ const SFOrdersStatus: React.FC<SFBuyProps> = ({ orderId }) => {
   const { order, loadingOrder, orderNotFound, orderFetchError } =
     useOrder(orderId);
 
-  const primaryColor = usePrimaryColor(order, orderNotFound, loadingOrder);
+  const primaryColor = usePrimaryColor(
+    order,
+    orderNotFound,
+    loadingOrder,
+    orderFetchError,
+  );
 
   return (
     <Box
@@ -39,18 +46,33 @@ const SFOrdersStatus: React.FC<SFBuyProps> = ({ orderId }) => {
           <Spinner type="dots" />
         </Box>
       ) : (
-        <OrderStatusInfo
-          order={order}
-          orderNotFound={orderNotFound}
-          orderFetchError={orderFetchError}
-          orderId={orderId}
-        />
+        <>
+          <InfoBlrub
+            order={order}
+            orderNotFound={orderNotFound}
+            orderFetchError={orderFetchError}
+            orderId={orderId}
+          />
+          <OrderBreakdown order={order} />
+        </>
       )}
     </Box>
   );
 };
 
-const OrderStatusInfo = ({
+const OrderBreakdown = ({ order }: { order: Nullable<HydratedOrder> }) => {
+  if (!order) {
+    return null;
+  }
+
+  return (
+    <Box marginTop={1}>
+      <Text>Order breakdown</Text>
+    </Box>
+  );
+};
+
+const InfoBlrub = ({
   order,
   orderNotFound,
   orderFetchError,
@@ -61,7 +83,8 @@ const OrderStatusInfo = ({
   orderFetchError: Nullable<ApiError>;
   orderId: string;
 }) => {
-  const Content = () => {
+  const InfoBlurb = () => {
+    // error states
     if (orderNotFound) {
       return (
         <Box flexDirection="column">
@@ -77,13 +100,67 @@ const OrderStatusInfo = ({
         </Box>
       );
     }
+    if (orderFetchError) {
+      return <Text color="red">{orderFetchError.message}</Text>;
+    }
+
+    // check order status
+    if (order) {
+      const endsAtIso = dayjs(order.start_at)
+        .add(order.duration, "seconds")
+        .toISOString();
+      const endAtLabelFormatted = dayjs(endsAtIso).format(
+        "ddd MMM D [at] h:mma",
+      );
+
+      switch (order.status) {
+        case OrderStatus.Pending:
+          return (
+            <Text>
+              This order is in a <Text color="blue">pending</Text> state. You
+              may have just submitted it, it will soon be on the market.
+            </Text>
+          );
+        case OrderStatus.Rejected:
+          return (
+            <Text>This order was rejected for some validation reason.</Text>
+          );
+        case OrderStatus.Open:
+          return (
+            <Text>
+              This order is <Text color="green">open</Text> and on the market.
+              It can be filled at any moment. It will expire on{" "}
+              <Text color="yellow">{endAtLabelFormatted}</Text>.
+            </Text>
+          );
+        case OrderStatus.Cancelled: {
+          return (
+            <Text>
+              This order was cancelled. It is no longer on the market.
+            </Text>
+          );
+        }
+        case OrderStatus.Filled:
+          // TODO: if a buy order, show the contract id the user now owns
+          return (
+            <Box flexDirection="column">
+              <Text>
+                This order was successfully filled for a price of{" "}
+                {centicentsToDollarsFormatted(order.execution_price!)} 🎉
+              </Text>
+            </Box>
+          );
+        case OrderStatus.Expired:
+          return <Text>This order has expired.</Text>;
+      }
+    }
 
     return null;
   };
 
   return (
     <Box marginTop={1}>
-      <Content />
+      <InfoBlurb />
     </Box>
   );
 };
@@ -92,16 +169,23 @@ const usePrimaryColor = (
   order: Nullable<HydratedOrder>,
   notFound: Nullable<boolean>,
   loadingOrder: boolean,
+  orderFetchError: Nullable<ApiError>,
 ) => {
   if (loadingOrder) {
     return "white";
   }
 
+  // error states
   if (notFound) {
     return "yellow";
   }
+  if (orderFetchError) {
+    return "red";
+  }
+
+  // should never happen (order found but not resolved)
   if (!order) {
-    return "gray"; // should never happen (order found but not resolved)
+    return "gray";
   }
 
   // check order status
