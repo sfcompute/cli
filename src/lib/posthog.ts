a9d6f1689eba9ce68e480ed1902cde12bdfc2310
John Pham <johnphammail@gmail.com>
2025-01-08 11:08:28 -0800

    Install posthog and setup client

---
+++ src/lib/posthog.ts
@@ -0,0 +1,8 @@
+import { PostHog } from "posthog-node";
+
+export const postHogClient = new PostHog(
+  "phc_ErsIQYNj6gPFTkHfupfuUGeKjabwtk3WTPdkTDktbU4",
+  {
+    host: "https://us.posthog.com",
+  }
+);
