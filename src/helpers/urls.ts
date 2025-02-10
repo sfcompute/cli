c4b39a9bfd762195e7530591bea787ee6dff1466
Daniel Tao <danieltaox@gmail.com>
2025-02-09 21:43:32 -0800

    style: strongly type `urls.ts` helper

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -1,63 +1,70 @@
 import { loadConfig } from "./config.ts";
 
-const webPaths = {
+type PathFunction<T> = (params: T) => string;
+type Path<T = unknown> = string | PathFunction<T>;
+
+type TokenParams = {
+  token: string;
+};
+
+type IdParams = {
+  id: string;
+};
+
+const webPaths: Record<string, Path<TokenParams | never>> = {
   cli_session_create: "/cli/session",
-  cli_session_get: ({ token }: { token: string }) =>
+  cli_session_get: ({ token }: TokenParams): string =>
     `/cli/session?token=${token}`,
 };
 
-const apiPaths = {
+const apiPaths: Record<string, Path<IdParams | never>> = {
   index: "/",
   me: "/v0/me",
   ping: "/v0/ping",
 
   orders_create: "/v0/orders",
   orders_list: "/v0/orders",
-  orders_get: ({ id }: { id: string }) => `/v0/orders/${id}`,
-  orders_cancel: ({ id }: { id: string }) => `/v0/orders/${id}`,
+  orders_get: ({ id }: IdParams): string => `/v0/orders/${id}`,
+  orders_cancel: ({ id }: IdParams): string => `/v0/orders/${id}`,
 
   quote_get: "/v0/quote",
 
-  instances_list: "/v0/instances",
-  instances_get: ({ id }: { id: string }) => `/v0/instances/${id}`,
+  instances_list: "/v0/instances", 
+  instances_get: ({ id }: IdParams): string => `/v0/instances/${id}`,
 
   credentials_create: "/v0/credentials",
   credentials_list: "/v0/credentials",
 
   contracts_list: "/v0/contracts",
-  contracts_get: ({ id }: { id: string }) => `/v0/contracts/${id}`,
+  contracts_get: ({ id }: IdParams): string => `/v0/contracts/${id}`,
 
   balance_get: "/v0/balance",
 
   tokens_create: "/v0/tokens",
   tokens_list: "/v0/tokens",
-  tokens_delete_by_id: ({ id }: { id: string }) => `/v0/tokens/${id}`,
+  tokens_delete_by_id: ({ id }: IdParams): string => `/v0/tokens/${id}`,
 };
 
-// --
-
-export async function getWebAppUrl(
+export async function getWebAppUrl<T extends TokenParams | never>(
   key: keyof typeof webPaths,
-  params?: any,
+  params?: T
 ): Promise<string> {
   const config = await loadConfig();
   const path = webPaths[key];
   if (typeof path === "function") {
-    return config.webapp_url + path(params);
+    return `${config.webapp_url}${params ? path(params) : ""}`;
   }
-
-  return config.webapp_url + path;
+  return `${config.webapp_url}${path}`;
 }
 
-export async function getApiUrl(
+export async function getApiUrl<T extends IdParams | never>(
   key: keyof typeof apiPaths,
-  params?: any,
+  params?: T
 ): Promise<string> {
   const config = await loadConfig();
   const path = apiPaths[key];
   if (typeof path === "function") {
-    return config.api_url + path(params);
+    return `${config.api_url}${params ? path(params) : ""}`;
   }
-
-  return config.api_url + path;
+  return `${config.api_url}${path}`;
 }
