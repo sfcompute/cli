f75d9b311a8566fb92195d51653761e3479bf8e2
Daniel Tao <danieltaox@gmail.com>
2025-05-14 15:07:56 -0700

    feat: [PRODUCT-409] show human friendly instance type in `sf scale` (#152)
    * feat: [PRODUCT-409] show human friendly instance type in `sf scale`
    
    * fix: show unchanged if instance type is unchanged
    
    * feat: update ConfirmationMessage based on feedback
    
    * feat: dim `"(1.5x market)"` text
    
    * feat: only `justify-between` if multiple rows have dim text

--- src/lib/scale/ConfirmationMessage.tsx
+++ src/lib/scale/ConfirmationMessage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Box, Text } from "ink";
 
+import { InstanceTypeMetadata } from "../../helpers/instance-types-meta.ts";
+
 import { Row } from "../Row.tsx";
 import { formatDuration } from "../orders/index.tsx";
 
@@ -22,7 +24,11 @@ export default function ConfirmationMessage(props: {
   const horizonInMilliseconds = props.horizonMinutes
     ? Math.max(props.horizonMinutes, MIN_CONTRACT_MINUTES) * 60 * 1000
     : undefined;
-
+  const isSupportedType = typeof props.type === "string" &&
+    props.type in InstanceTypeMetadata;
+  const typeLabel = isSupportedType
+    ? InstanceTypeMetadata[props.type!].displayName
+    : props.type;
   return (
     <Box flexDirection="column" marginBottom={1}>
       <Box gap={1}>
@@ -34,7 +40,18 @@ export default function ConfirmationMessage(props: {
       <Row
         headWidth={30}
         head="Type"
-        value={props.type ?? "unchanged"}
+        value={isSupportedType
+          ? (
+            <Box
+              gap={1}
+              minWidth={props.quote ? 30 : undefined}
+              justifyContent="space-between"
+            >
+              <Text>{typeLabel}</Text>
+              <Text dimColor>({props.type})</Text>
+            </Box>
+          )
+          : "unchanged"}
       />
       <Row
         headWidth={30}
@@ -45,13 +62,16 @@ export default function ConfirmationMessage(props: {
       />
       <Row
         headWidth={30}
-        head={`Limit Price${
-          (props.quote && props.pricePerGpuHourInCents !== undefined)
-            ? " (1.5 x market)"
-            : ""
-        }`}
+        head="Limit Price"
         value={props.pricePerGpuHourInCents !== undefined
-          ? `$${(props.pricePerGpuHourInCents / 100).toFixed(2)}/gpu/hr`
+          ? (
+            <Box gap={1} minWidth={30} justifyContent="space-between">
+              <Text>
+                ${(props.pricePerGpuHourInCents / 100).toFixed(2)}/gpu/hr
+              </Text>
+              {props.quote && <Text dimColor>(1.5x market)</Text>}
+            </Box>
+          )
           : "unchanged"}
       />
       <Row
