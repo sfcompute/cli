e1a19f64d3f4110c5ca98309a83553fda9a1bf5d
Daniel Tao <danieltaox@gmail.com>
2025-10-10 01:22:16 +0900

    feat: mount ssh and logs to sf nodes as subcommands

--- src/lib/nodes/index.ts
+++ src/lib/nodes/index.ts
@@ -9,6 +9,8 @@ import set from "./set.ts";
 import extend from "./extend.ts";
 import get from "./get.tsx";
 import { addRedeploy } from "./redeploy.ts";
+import ssh from "./ssh.ts";
+import logs from "./logs.ts";
 import { isFeatureEnabled } from "../posthog.ts";
 
 export async function registerNodes(program: Command) {
@@ -25,7 +27,9 @@ export async function registerNodes(program: Command) {
     .addCommand(extend)
     .addCommand(release)
     .addCommand(deleteCommand)
-    .addCommand(set);
+    .addCommand(set)
+    .addCommand(ssh)
+    .addCommand(logs);
 
   const baseHelpText = nodes.helpInformation();
 
@@ -64,6 +68,18 @@ $ sf nodes set my-node-name --max-price 12.50
 
 \x1b[2m# Extend a reserved node\x1b[0m
 $ sf nodes extend my-node-name --duration 3600 --max-price 12.50
+
+\x1b[2m# SSH into a node's current VM\x1b[0m
+$ sf nodes ssh my-node-name
+
+\x1b[2m# View logs from a node's current VM\x1b[0m
+$ sf nodes logs my-node-name
+
+\x1b[2m# SSH into a specific VM\x1b[0m
+$ sf nodes ssh user@vm_xxxxxxxxxxxxxxxxxxxxx
+
+\x1b[2m# View logs from a specific VM\x1b[0m
+$ sf nodes logs -i vm_xxxxxxxxxxxxxxxxxxxxx
   `,
     )
     // Add action to display help if no subcommand is provided
