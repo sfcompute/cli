78eb17f88f143587c7aa8e73118c301b33b4ffd9
Daniel Tao <danieltaox@gmail.com>
2025-12-09 18:52:04 -0800

    feat: add deprecation warning banner to `sf vms`

--- src/lib/vm/index.ts
+++ src/lib/vm/index.ts
@@ -1,4 +1,9 @@
-import { type Command } from "@commander-js/extra-typings";
+import { Command } from "@commander-js/extra-typings";
+import { red, yellow } from "jsr:@std/fmt/colors";
+import boxen from "boxen";
+import console from "node:console";
+import { nodesClient } from "../../nodesClient.ts";
+import { pluralizeNodes } from "../nodes/utils.ts";
 import { registerSsh } from "./ssh.ts";
 import { addImage } from "../nodes/image/index.ts";
 import list from "./list.ts";
@@ -6,12 +11,49 @@ import logs from "./logs.ts";
 import replace from "./replace.ts";
 import script from "./script.ts";
 
+const DEPRECATION_WARNING = red(boxen(
+  `\x1b[31mWe're deprecating \x1b[37msf buy\x1b[31m and \x1b[37msf vm\x1b[31m for Virtual Machines.\x1b[31m
+\x1b[31mWe recommend you create a VM Node instead: \x1b[37msf nodes create --help\x1b[31m
+\x1b[37msf nodes\x1b[31m allows you to create, extend, and release specific machines directly.\x1b[31m`,
+  {
+    padding: 0.75,
+    borderColor: "red",
+  },
+));
+
+async function getVMDeprecationWarning() {
+  const client = await nodesClient();
+  const nodes = await client.nodes.list();
+  const numNodesWithVMs =
+    nodes.data?.filter((node) => node.vms?.data?.length ?? 0 > 0).length ?? 0;
+  if (numNodesWithVMs > 0) {
+    return yellow(
+      boxen(
+        `You have \x1b[1m\x1b[33m${numNodesWithVMs} ${
+          pluralizeNodes(numNodesWithVMs)
+        }\x1b[0m\x1b[33m with active or previous VMs.
+Managing VM Nodes with deprecated \x1b[37msf vm\x1b[33m commands may cause undefined behavior.
+Use \x1b[37msf nodes\x1b[33m to create, extend and release specific machines directly.`,
+        {
+          padding: 0.75,
+          borderColor: "yellow",
+        },
+      ),
+    );
+  }
+  return DEPRECATION_WARNING;
+}
 export async function registerVM(program: Command) {
   const vm = program
     .command("vm")
     .showHelpAfterError()
     .aliases(["v", "vms"])
-    .description("Manage virtual machines");
+    .description("Manage virtual machines")
+    .hook("preAction", async () => {
+      const vmDeprecationWarning = await getVMDeprecationWarning();
+      console.error(vmDeprecationWarning);
+    })
+    .addHelpText("beforeAll", DEPRECATION_WARNING + "\n");
 
   registerSsh(vm);
 
